89d05c1c2dd1db2285705205c152fba3c3031a63
gaikwadomg <connect2ompc@gmail.com>
2024-02-16 23:58:16 +0530

    learned globals in node like __dirname , module, console.log , __filename , etc

---
+++ Basic Programs/globals.js
@@ -0,0 +1,17 @@
+
+
+
+
+console.log("this is my directory -->" ,__dirname);
+console.log("this is my filename -->" ,__filename);
+console.log("this is my require -->" ,require);
+console.log("this is my module -->" ,module);
+console.log("this is my process -->" ,process);
+
+setInterval(() => {
+    console.log("Hello World !");
+},1000);
+
+// setInterval(() =>{
+//     console.log("hi");
+// },2000);
