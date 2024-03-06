f1c6594f1f46c3bcddbfb2276d3ef18b29375d8b
gaikwadomg <connect2ompc@gmail.com>
2024-03-06 22:39:47 +0530

    added files

--- Basic Programs/app.js
+++
@@ -1,7 +0,0 @@
-console.log("hello om gikwad");
-
-const _ = require('lodash');
-const item = [1,[2,[3,[4]]]];
-
-const newItem = _.flattenDeep(item);
-console.log(newItem);
---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hello om gaikwad !");
+
+const _ = require('lodash');
+const item = [1,[2,[3,[21]]]];
+
+const newItem = _.flattenDeep(item);
+console.log(newItem);
+console.log("Om Gaikwad !");
